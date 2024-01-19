5e94859d1b92a441d438317d69f56188555a63f9
kristjanpeterson <kristjan@nftport.xyz>
2024-01-19 11:16:59 +0200

    init

--- js/index.js
+++ js/index.js
@@ -5,10 +5,12 @@ import {TransactionBlock} from "@mysten/sui.js/transactions";
 import {bcs} from "@mysten/sui.js/bcs";
 import {fromB64} from "@mysten/bcs";
 
+// YOur own wallet address and key
 const MY_ADDRESS = "0x3d13424a38ba62a9e0155c78b44ffb717b8612e1025f3d56bd4592d57a008c5f"
 const PRIVATE_KEY = "0x279b4815bca2414dfad94c6f72ee3191c04c64fd8d839396ba0de669a630fcc7"
 
 const ADDRESS_OWNER = "0xa889ea8ef488a5d34fd2691b1982954958e0730c1b8d31cca91ffb346de56b41"
+// Get address with `sui client addresses` and extract private key with keychain
 const PRIVATE_KEY_OWNER = "02e5f4af751c878fc74669a886eb00d7489dfc988a3587937d6539f1aab61224" // no 0x
 
 const PACKAGE_ID = "0x3fb7e588aa0894d205ef78b32b3f3a188cdf292571b31177be5ccd6666d514fa"
